c01d5db80a03e947257e6d47fa87db3c0bf67fde
Arturo Ciaceri <arturo.ciaceri@gmail.com>
2022-03-22 17:38:35 +0100

    adding Id method to Utente class

--- Utente.java
+++ Utente.java
@@ -1,5 +1,10 @@
 public class Utente {
     public String nome;
     public String cognome;
+    public int età;
+
+    public void generateId(){
+        System.out.println(nome + cognome);
+    }
 
 }
